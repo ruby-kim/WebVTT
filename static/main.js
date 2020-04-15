423df40172f0e05b0c3994f9b0c03bc7fc006252
kimkyeongnam <kkyy0126@naver.com>
2020-04-15 13:06:22 +0900

    [Add] HTMLMediaElement.textTracks

--- static/main.js
+++ static/main.js
@@ -1,1 +1,10 @@
- 
+$(document).ready(function () {
+    var player = document.querySelector("#customVideo");
+    var tracks = player.textTracks[0];
+    tracks.oncuechange = function (e) {
+        var cue = tracks.activeCues[0];
+        if (cue) {
+            $("#vttText").text(cue.text);
+        }
+    }
+});
